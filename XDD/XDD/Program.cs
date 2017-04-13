800002f4f556b1a8a957e0b2dcb65aba810e4101
floatGOAT <pandarpr@yandex.ru>
2017-04-13 18:40:25 +0600

    BDD++ #2 - Quick Sort

--- XDD/XDD/Program.cs
+++ XDD/XDD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,46 @@ namespace XDD
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(Tests_qsort.TestIn3());
+            Console.WriteLine(Tests_qsort.TestInSameNumbers());
+            Console.WriteLine(Tests_qsort.TestInEmptyArray());
+            Console.WriteLine(Tests_qsort.TestIn1000());
+            // Summon the Devil 
+            // Console.WriteLine(Tests_qsort.StressTest());
+            Console.ReadKey();
+        }
+
+        public static void qsort(ref int[] arr, int low, int high)
+        {
+            if (low > high)
+                return;
+            int i = low;
+            int j = high;
+            int median = Median(arr[low], arr[high], arr[(low + high) / 2]);
+            while (i < j)
+            {
+                while (i < j && arr[i] <= median)
+                    i++;
+                while (j >= i && arr[j] > median)
+                    j--;
+                if (i < j)
+                {
+                    arr[i] += arr[j];
+                    arr[j] = arr[i] - arr[j];
+                    arr[i] = arr[i] - arr[j];
+                }
+            }
+            if (i - low > 0) 
+                qsort(ref arr, low, i - 1);
+            if (high - i > 1)
+                qsort(ref arr, i, high);
+            return;
+        }
+
+        public static int Median(int a, int b, int c)
+        {
+            return (a + b + c) / 3;
         }
     }
 }
---
+++ XDD/XDD/Tests_qsort.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace XDD
+{
+    class Tests_qsort
+    {
+        public static string TestIn3()
+        {
+            int[] array = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                array[i] = new Random().Next(0, 1000);
+            }
+            Program.qsort(ref array, 0, array.Length - 1);
+            bool ret = array[0] <= array[1] && array[1] <= array[2];
+            Console.WriteLine(array[0] + " " + array[1] + " "+ array[2]);
+            return ret ? "Тест на 3 числах пройден." : "Тест на 3 числах не пройден.";
+        }
+
+        public static string TestInSameNumbers()
+        {
+            int[] array = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = 42;
+            }
+            Program.qsort(ref array, 0, array.Length - 1);
+            return AreWeRight(ref array) ? "Тест на одинаковых числах пройден." : "Тест на одинаковых числах не пройден.";
+        }
+
+        public static string TestIn1000()
+        {
+            bool Flag = true;
+            int[] array = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                array[i] = new Random().Next(0, 10000);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int A = new Random().Next(0, 999);
+                int B = new Random().Next(0, 999);
+                if (array[Math.Min(A, B)] > array[Math.Max(A, B)])
+                    Flag = false;
+            }
+            Program.qsort(ref array, 0, array.Length - 1);
+            return Flag ? "Тест на одинаковых числах пройден." : "Тест на одинаковых числах не пройден.";
+        }
+
+        public static string TestInEmptyArray()
+        {
+            int[] array = new int[0];
+            Program.qsort(ref array, 0, array.Length - 1);
+            return AreWeRight(ref array) ? "Тест в пустом массиве пройден." : "Тест в пустом массиве не пройден.";
+        }
+
+        public static string StressTest()
+        {
+            int SIZE = 1500000000;
+            int[] array = new int[SIZE];
+            for (int i = 0; i < SIZE; i++)
+            {
+                array[i] = new Random().Next();
+            }
+            Program.qsort(ref array, 0, array.Length - 1);
+            return AreWeRight(ref array) ? "Стресс-тест пройден." : "Стресс-тест не пройден.";
+        }
+
+        private static bool AreWeRight(ref int[] array)
+        {
+            bool flag = true;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                    flag = false;
+            }
+            return flag;
+        }
+    }
+}
